aac084b3a44ff92009c43a5814a181d5ed91fdee
Joel Kemp <jokemp@adobe.com>
2015-02-10 14:37:10 -0500

    Remove jquery from stacktrace

--- network.js
+++ network.js
@@ -3,7 +3,7 @@
  * @return {String}
  */
 module.exports = function(stackTrace) {
-  var filterRegex = /grunt-behance/;
+  var filterRegex = /(grunt-behance|jquery.js)/;
 
   return stackTrace.split('\n').filter(function(line) {
     return !filterRegex.test(line);
